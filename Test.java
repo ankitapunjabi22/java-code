bb098203abb697c006ed0268a7d76d1b40172f45
ankitapunjabi22 <2024.ankita.punjabi@ves.ac.in>
2025-09-24 09:46:24 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
-     piblic static void main(String[] args) {
+     public static void main(String[] args) {
           System.out.println("hello world");
           System.out.println("hello from GITHUB");
      }
